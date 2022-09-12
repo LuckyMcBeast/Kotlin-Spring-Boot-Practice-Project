0340a3a6dd85ad3fe8500fd17d6d605c2121d919
Kieran Murphy <kimurphy@thoughtworks.com>
2022-09-12 15:21:12 -0500

    Kieran, Cullen | adds let block in ping test

--- src/test/kotlin/com/kandc/practice/PracticeControllerTest.kt
+++ src/test/kotlin/com/kandc/practice/PracticeControllerTest.kt
@@ -5,29 +5,27 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
-import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.get
 
-@SpringBootTest
+@WebMvcTest
 @AutoConfigureMockMvc
 class PracticeControllerTest {
-
-    @Autowired
-    lateinit var practiceController: PracticeController
 
     @Autowired
     private lateinit var mockMvc: MockMvc
 
     @Test
-    fun `should get a 200 from ping`(){
+    fun `ping gets 200 and LIVE`(){
 
-        assertThat(mockMvc.get("/ping")
-            .andDo { print() }
-            .andReturn().response.status)
-            .isEqualTo(200)
-
+        mockMvc.get("/ping")
+                .andDo { print() }
+                .andReturn()
+                .response
+                .let { response ->
+                    assertThat(response.status).isEqualTo(200)
+                    assertThat(response.contentAsString).isEqualTo("LIVE")
+                }
     }
-
-
 }
